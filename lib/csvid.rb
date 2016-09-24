2d9f867072f805d6fd6b93c12039115d89be4c0d
zaru@sakuraba <zarutofu@gmail.com>
2016-09-24 23:12:33 +0900

    add methods

--- lib/csvid.rb
+++ lib/csvid.rb
@@ -1,5 +1,37 @@
 require "csvid/version"
+require 'csv'
 
 module Csvid
-  # Your code goes here...
+  module ClassMethods
+    def csv_path(path)
+      @csv_path = path
+    end
+
+    def all
+      results = []
+      head, *body = CSV.read(@csv_path, encoding: "CP932")
+
+      head.each_with_index do |field, index|
+        define_method field do
+          @line[index]
+        end
+      end
+
+      body.each do |line|
+        results << self.new(line)
+      end
+
+      results
+    end
+  end
+
+  class << self
+    def included(base)
+      base.extend(ClassMethods)
+    end
+  end
+
+  def initialize(line)
+    @line = line
+  end
 end
